e891f9746165a23233a09e770f8e038b55471dca
mutech23 <olawalemujeeb123@gmail.com>
2025-07-14 21:53:37 -0700

    Update seervice-worker.js

--- seervice-worker.js
+++ seervice-worker.js
@@ -1,21 +1,18 @@
-// Install the service worker and cache the site
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('adhan-cache-v1').then(cache => {
+    caches.open('v1').then(cache => {
       return cache.addAll([
         '/',
         '/index.html',
-        '/manifest.json',
-        '/service-worker.js',
+        '/style.css',
         '/pngwing.com.png',
-       
-        // Add more files if needed (e.g. CSS, JS)
+        // Add other assets here
       ]);
     })
   );
+  self.skipWaiting();
 });
 
-// Fetch from cache if offline
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
